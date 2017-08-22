d3b397afb5e6e63ca4f81abfd206bd6d69caebeb
Juan David Ocampo Arias <Jocampoa@intergrupo.com>
2017-08-21 19:00:21 -0500

    se cambio la Views en ForeignExchangePage

--- ForeignExchangeWin1/ForeignExchangeWin1/ViewModels/MainViewModel.cs
+++ ForeignExchangeWin1/ForeignExchangeWin1/ViewModels/MainViewModel.cs
@@ -8,6 +8,10 @@
     using System.Collections.ObjectModel;
     using Models;
     using Services;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
     public class MainViewModel : INotifyPropertyChanged
     {
         #region "Events"
@@ -23,6 +27,7 @@
         string _euros;
         string _pounds;
         ObservableCollection<Rate> _rates;
+        string _result;
         bool _isEnabled;
         bool _isRunning;
         int _sourceRateID;
@@ -31,24 +36,79 @@
         #endregion
 
         #region "Properties"
-        public ObservableCollection<Rate> Rates { get; set; }
-
-        public int SourceRateID { get; set; }
-
-        public int TargetRateID { get; set; }
-
-        public bool IsEnabled { get; set; }
-
-        public bool IsRunning { get; set; }
-
-        public string Result { get; set; }
-        public string Pesos
+        public string Amount { get; set; }
+        public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
         }
 
-        public string Amount
+        public Rate SourceRateID { get; set; }
+
+        public Rate TargetRateID { get; set; }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(this,
+                        new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+        }
+        public string Pesos
         {
             get;
             set;
@@ -110,10 +170,35 @@
         async void LoadRates()
         {
             IsRunning = true;
+            Result = "Loading rates...";
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://apiexchangerates.azurewebsites.net");
+                var controller = "/api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+                IsRunning = false;
+                IsEnabled = true;
+                Result = "Ready to convert!";
+            }
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message;              
+            }
+
             IsEnabled = false;
-            var response = await apiServices.GetRates();
-            IsRunning = false;
-            IsEnabled = true;
+           
+
         }
         #endregion
 
@@ -154,12 +239,51 @@
             {
                 return new RelayCommand(ConverterPlus);
             }
-            #endregion           
         }
-
-        void ConverterPlus()
+        async void ConverterPlus()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(Amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a value in amount.",
+                    "Accept");
+                return;
+            }
+
+            decimal amount = 0;
+            if(!decimal.TryParse(Amount, out amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                "You must enter a numeric value in amount.",
+                "Accept");
+                return;
+            }
+
+            if (SourceRateID == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a source rate",
+                    "Accept");
+                return;
+            }
+
+            if (TargetRateID == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a target rate",
+                    "Accept");
+                return;
+            }
+
+            var amountConverted = amount / (decimal)SourceRateID.TaxRate * (decimal)TargetRateID.TaxRate;
+            Result = string.Format("{0} {1:C2} = {2} {3:C2}",
+                SourceRateID.Code, amount, TargetRateID.Code, amountConverted);
         }
+        #endregion
     }
 }
+
